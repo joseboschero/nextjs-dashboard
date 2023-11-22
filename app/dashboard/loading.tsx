cb9bb9be8fb5cf0628b06e2b290fa1b7597ae21f
joseboschero <joseboscherodev@gmail.com>
2023-11-22 13:46:48 -0300

    ended lession

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,7 +1,5 @@
+import DashboardSkeleton from '../ui/skeletons';
+
 export default function Loading() {
-  return (
-    <div>
-      <p>Cargando datos . . .</p>
-    </div>
-  );
+  return <DashboardSkeleton />;
 }
