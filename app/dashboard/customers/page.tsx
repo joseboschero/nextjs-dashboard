3e3a0ab557ed9020041231af70027d8495049476
joseboschero <joseboscherodev@gmail.com>
2023-11-21 21:03:58 -0300

    learned next js

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+  return (
+    <>
+      <div>
+        <p>Customers Page</p>
+      </div>
+    </>
+  );
+}
---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '500'],
+});
+
+export const lusitana = Lusitana({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+});
